d41e6013f503ea69c21ad840add9dc58892dfca4
faty <fatyboubakri31@gmail.com>
2023-12-23 00:40:52 +0000

    task 1 python3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #  install flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
+  require  => Package['python3-pip'],
 }
